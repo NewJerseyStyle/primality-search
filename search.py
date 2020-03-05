dc87246c0ec43a691b58a18148178c624a057c6b
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2020-03-06 00:11:45 +0800

    Update search.py

--- search.py
+++ search.py
@@ -1,5 +1,5 @@
 from os import path
-from gmpy2 import mpz, is_prime, bit_length
+from gmpy2 import mpz, is_prime, bit_length, next_prime
 import pickle
 from aks import aks_test
 
@@ -7,18 +7,24 @@ if path.exists('p.pkl')
 	with open('p.pkl', 'rb') as f:
 		p = pickle.load(f)
 else:
-	known_largest_prime_power = 82589933
-	searching_begin_power = known_largest_prime_power * 5
-	p = mpz(2) ** searching_begin_power
+	known_largest_prime_power = mpz(82589933)
+	searching_power = known_largest_prime_power * 5
+	p = mpz(2) ** searching_power
 
 	with open('p.pkl', 'wb') as f:
 		pickle.dump(p, f)
 
+
+pre_p = p
+pre_searching_power = searching_power
 while True:
-	t = p - 1
-	if is_prime(t):
-		if aks_test(t) == "prime":
-			print('Find interesting thing: 2**%d-1' %bit_length(t))
-			with open('findings.txt', 'a') as f:
-				f.write('2**%d-1\n' %bit_length(t))
-	p *= 2
+	if aks_test(searching_power) == "prime":
+		p *= mpz(2)**(searching_power-pre_searching_power)
+		pre_searching_power = searching_power
+		t = p - 1
+		if is_prime(t):
+			if aks_test(t) == "prime":
+				print('Find interesting thing: 2**%s-1' %searching_power)
+				with open('findings.txt', 'a') as f:
+					f.write('2**%s-1\n' %searching_power)
+	searching_power = next_prime(searching_power)
