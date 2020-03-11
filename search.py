241898919e9eb32a539c65359bdc790a22d34cd6
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2020-03-11 11:25:49 +0800

    Update search.py

--- search.py
+++ search.py
@@ -1,15 +1,15 @@
 from os import path
-from gmpy2 import mpz, is_prime
+from gmpy2 import mpz, is_prime, log
 import pickle
 from aks import aks_test
 import ray
 
 @ray.remote
-def search_range(x, r=8192):
+def search_range(x, step=2):
 	if x & 1 == 0:
 		x += 1
 	return_val = []
-	for i in range(x, x + r, 2):
+	for i in range(x, x + 2*log(x), step):
 		if is_prime(i):
 			return_val.append(i)
 	return return_val
@@ -18,17 +18,9 @@ def search_range(x, r=8192):
 def next_prime(x, num_workers=31):
 	# search range split
 	jobs = []
-	while True:
-		for i in range(x, x + num_workers, 2):
-			jobs.append(search_range.remote(i))
-		result = ray.get(jobs)
-		return_val = []
-		for i in result:
-			for j in i:
-				if j:
-					return_val.append(j)
-		if len(return_val):
-			return return_val
+	for i in range(num_workers):
+		jobs.append(search_range.remote(x + i*2, num_workers*2))
+	return [i for e in ray.get(jobs) for i in e]
 
 
 ray.init()
@@ -45,16 +37,17 @@ else:
 		pickle.dump(p, f)
 
 
-pre_p = p
+searching_powers = [searching_power]
 pre_searching_power = searching_power
 while True:
-	if aks_test(searching_power) == "prime":
-		p *= mpz(2)**(searching_power-pre_searching_power)
-		pre_searching_power = searching_power
-		t = p - 1
-		if is_prime(t):
-			if aks_test(t) == "prime":
-				print('Find interesting thing: 2**%s-1' %searching_power)
-				with open('findings.txt', 'a') as f:
-					f.write('2**%s-1\n' %searching_power)
-	searching_power = next_prime(searching_power)
+	for searching_power in searching_powers:
+		if aks_test(searching_power) == "prime":
+			p *= mpz(2)**(searching_power-pre_searching_power)
+			pre_searching_power = searching_power
+			t = p - 1
+			if is_prime(t):
+				if aks_test(t) == "prime":
+					print('Find interesting thing: 2**%s-1' %searching_power)
+					with open('findings.txt', 'a') as f:
+						f.write('2**%s-1\n' %searching_power)
+	searching_powers = next_prime(searching_powers[-1])
